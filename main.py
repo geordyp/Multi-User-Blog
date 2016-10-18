e9f7676ce4c4ef231ceab51affc3638decdcccbc
geordyp <geowilliams6@gmail.com>
2016-10-18 18:57:40 -0500

    Add html template
    Added jinja2 to support html templates.

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
+import os
+import jinja2
 import webapp2
 
+template_dir = os.path.join(os.path.dirname(__file__), "html")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
-class MainPage(webapp2.RequestHandler):
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.render("base.html")
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ("/", MainPage),
 ], debug=True)
