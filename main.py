dd038037aa048726a1cc781f1aa8c677fa3d63af
geordyp <geowilliams6@gmail.com>
2016-10-19 00:52:03 -0500

    Push new posts to database

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import jinja2
 import webapp2
 
+from google.appengine.ext import db
+
 template_dir = os.path.join(os.path.dirname(__file__), "html")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
@@ -16,6 +18,15 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+
+class Post(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
 class MainPage(Handler):
     def get(self):
         self.render("base.html")
@@ -29,12 +40,14 @@ class NewPostHandler(Handler):
         content = self.request.get("content")
 
         if subject and content:
-            self.redirect("/blog/")
+            p = Post(parent = blog_key(), subject=subject, content=content)
+            p.put()
         else:
             error = "subject and content, please!"
             self.render("newpost.html", subject=subject, content=content, error=error)
 
 app = webapp2.WSGIApplication([
     ("/blog/?", MainPage),
-    ("/blog/newpost", NewPostHandler)
+    ("/blog/newpost", NewPostHandler),
+    ("/blog/([0-9]+)", PostPageHandler)
 ], debug=True)
